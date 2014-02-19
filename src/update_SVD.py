9aeeeb24bc5c65796e3ebaef299d619cdaa4c558
Matt <mtbarta@gmail.com>
2014-02-18 19:25:29 -0600

    extending SVD for updates

---
+++ src/update_SVD.py
@@ -0,0 +1,9 @@
+import src.SVD as SVD
+
+class update_SVD(SVD):
+    """Checklist: 1. Use folding-up method to update predictions
+                2. store percent of new documents folded in, 
+                    determine when to update.
+                3. auto cross-validate for update response? """
+                
+
