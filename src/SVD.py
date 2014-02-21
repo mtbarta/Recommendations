be2f087d401a761990a1ec6214481292a618ecc6
Matt <mtbarta@gmail.com>
2014-02-21 17:29:54 -0600

    testing SVD

--- src/SVD.py
+++ src/SVD.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-
+# implement biases-- http://arek-paterek.com/ap_kdd_poster.pdf
 class SVD():
     def __init__(self, data, user_count, item_count, rank, learning_rate, reg):
         """data is a tuple of tuples (user, item, rating)"""
@@ -17,32 +17,33 @@ class SVD():
         noise = .005
         
         #items
-        self.U = np.random.uniform(-noise, noise, item_count*rank)\
+        self.V = np.random.uniform(-noise, noise, item_count*rank)\
                 .reshape(item_count,rank) + init
         #users
-        self.V = np.random.uniform(-noise,noise, user_count*rank)\
+        self.U = np.random.uniform(-noise,noise, user_count*rank)\
                 .reshape(user_count, rank) + init
                 
     def predict(self, userid, itemid):
         """rating of user i and item j is simply the dot product"""
-        prediction = 0
+        prediction = 0 #np.average(self.U[userid])
         for i in range(self.rank):
             prediction += self.U[userid][i] * self.V[itemid][i]
         return prediction
         
-    def train_model(self, data, epochs):
+    def train(self, data, epochs):
         oldtrain = 0
         for epoch in range(epochs):
-            train = self.train()
+            train = self.train_single(self.rank -1)
             
             if abs(oldtrain - train) < self.minimprove:
                 break
             oldtrain = train
             
         
-    def train(self, k):
+    def train_single(self, k):
         """iterate through the data. For each rating, update the user and item factors."""
         SSqerror = 0
+        
         for userid, item, rating in self.data:
             itemid = item-1
             
@@ -55,5 +56,5 @@ class SVD():
             self.U[userid][k] += self.learning_rate * (error * vTemp - self.reg * uTemp)
             self.V[itemid][k] += self.learning_rate * (error * uTemp - self.reg * vTemp)
             
-        return SSqerror/self.data.count
+        return SSqerror / len(self.data)
             
--- tests/SVD_tests.py
+++ tests/SVD_tests.py
@@ -1,5 +1,5 @@
 
-test_tuples = [(0,0,3),
+test_tuples = [(0,1,3),
                (1,1,2),
                (0,2,5),
                (3,4,1)]
@@ -10,4 +10,10 @@ def test_SVD_init():
     svd = SVD(test_tuples, 4, 5, 5, .035, .001)
     assert svd.U.shape == (5,5)
     assert svd.V.shape == (4,5)
-    
+
+svd = SVD(test_tuples, 4, 5, 5, .035, .001)
+def test_SVD():
+    svd.train(10, 10)
+    print svd.predict(0,2)
+
+test_SVD()
