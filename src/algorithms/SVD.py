2a78a0390c038e4ae8bc9bf5f03d22dbd0ad7dab
mtbarta <mtbarta@gmail.com>
2014-03-03 23:10:28 -0600

    corrected train and train_single

--- src/algorithms/SVD.py
+++ src/algorithms/SVD.py
@@ -2,7 +2,10 @@ import numpy as np
 from src.algorithms.algorithm import Algorithm
 # implement biases-- http://arek-paterek.com/ap_kdd_poster.pdf
 class SVD(Algorithm):
-    def __init__(self, data, user_count, item_count, rank, learning_rate, reg):
+    def __init__(self, data, user_count, item_count, 
+                 rank = 50, 
+                 learning_rate = .001, 
+                 reg = .05):
         """data is a tuple of tuples (user, item, rating)"""
         self.data = data
         self.rank = rank #factors
@@ -29,31 +32,35 @@ class SVD(Algorithm):
         """rating of user i and item j is simply the dot product"""
         prediction = 0 #np.average(self.U[userid])
         for i in range(self.rank):
-            prediction += self.U[userid][i] * self.V[itemid][i]
+            prediction += self.U[userid-1][i] * self.V[itemid-1][i]
         return prediction
         
     def train(self, epochs = 20):
+        print "starting training..."
         for epoch in range(epochs):
-            train = self.train_single(self.rank -1)
+            print "training -- epoch: %s" % epoch
+            train = self.train_single()
+            print "..... RMSE: %s" % train
             if np.abs(self.RMSE - train) < self.minimprove:
                 break
             self.RMSE = train
         
-    def train_single(self, k):
+    def train_single(self):
         """iterate through the data. For each rating, update the user and item factors."""
-        SSqerror = 0
+        SSqerror = 0.0
         
-        for userid, item, rating in self.data:
+        for user, item, rating in self.data:
             itemid = item-1
+            userid = user -1
             
-            error = rating - self.predict(userid, itemid)
+            error = rating - self.predict(user, item)
             SSqerror += error ** 2
-            
-            uTemp = self.U[userid][k]
-            vTemp = self.V[itemid][k]
-            
-            self.U[userid][k] += self.learning_rate * (error * vTemp - self.reg * uTemp)
-            self.V[itemid][k] += self.learning_rate * (error * uTemp - self.reg * vTemp)
-            
+            for rank in range(self.rank):
+                uTemp = self.U[userid][rank]
+                vTemp = self.V[itemid][rank]
+                
+                self.U[userid][rank] += self.learning_rate * (error * vTemp - self.reg * uTemp)
+                self.V[itemid][rank] += self.learning_rate * (error * uTemp - self.reg * vTemp)
+                
         return SSqerror / len(self.data)
             
