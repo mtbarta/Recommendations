6d552e1903d06c314b124d247b3f421835728c37
mtbarta <mtbarta@gmail.com>
2014-03-03 18:17:16 -0600

    properly closed database connection

--- src/data.py
+++ src/data.py
@@ -8,21 +8,27 @@ import MySQLdb
 
 class Database():
     def connect(self, host, user, passwd, db):
-        db = MySQLdb.connect(host = host, 
+        self.db = MySQLdb.connect(host = host, 
                              port = 3306, 
                              user= user, 
                              passwd = passwd,
                              db = db)
-        self.cursor = db.cursor();
+        self.cursor = self.db.cursor();
 
     def get_ratings(self):
-        if self.cursor:
+        try:
             self.cursor.execute("SELECT userid,itemid,rating "
                                 "FROM ratings")
             return self.cursor.fetchall()
            
-        else:
+        except:
+            print "database cursor not set for get_ratings() to read."\
+                    "Please make sure Database.connect() is configured."
             return
+        
+        finally:
+            self.cursor.close()
+            self.db.close()
         
     def get_movie(self, movie):
         if self.cursor:
